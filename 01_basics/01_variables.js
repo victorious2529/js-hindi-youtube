72524041c455d0a0b46218898296950cefa4fe62
victorious2529 <161911602+victorious2529@users.noreply.github.com>
2024-03-02 07:43:15 +0000

    variables basic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="example@google.com"
+var accountPassword="12345"
+accountCity="Delhi"
+console.table([accountId,accountEmail,accountCity]);
+// accountId=2 //const cannot be changed
+accountEmail="heihd@gmail.com"
+accountCity="Jaipur"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity]);
